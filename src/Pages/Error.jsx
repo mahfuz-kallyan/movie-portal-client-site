d8cc1c1f5bfc418454dc45164019f8471b474d2e
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-12-05 02:37:54 +0600

    added layputs

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h2>404</h2>
+            <h3>Page not Found</h3>
+        </div>
+    );
+};
+
+export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './Pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Error from './Pages/Error';
+import Root from './Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
