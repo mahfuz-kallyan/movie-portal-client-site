ec45e68185f0f19301751a8c2ae372b0078aa353
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-12-05 02:57:39 +0600

    added fonts

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div>
+        <div className='max-w-[1920px] mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
