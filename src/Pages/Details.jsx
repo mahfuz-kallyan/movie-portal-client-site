23887ebaab9729ab9e97876214aea0aeb39f53e3
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-12-12 03:15:25 +0600

    added delete option

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Details = () => {
     const movie = useLoaderData();
-    const {_id, poster, genre, title, duration, releaseYear, rating, summary } = movie;
+    const [movies, setMovies] = useState(movie)
+    const navigate = useNavigate()
+    const { _id, poster, genre, title, duration, releaseYear, rating, summary } = movie;
 
+    const handleDelete = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/movies/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your movie has been deleted.",
+                                icon: "success"
+                            });
+                            navigate('/movies')
+                            
+                            const remaining = movies.filter(mov => mov._id !== id)
+                            setMovies(remaining)
+                        }
+                    })
+
+            }
+        });
+    }
+    const handleClick = () => {
+        navigate('/movies')
+    }
     return (
         <div className='py-14'>
             <h2 className='text-center text-3xl font-semibold my-12'>Movie Details</h2>
@@ -23,8 +64,11 @@ const Details = () => {
                         <p className='text-lg font-medium'>Rating: <span className='text-red-500 font-semibold'>{rating}</span></p>
                         <p className='text-lg font-medium'>Release Year: <span className='text-red-500 font-semibold'>{releaseYear}</span></p>
                         <div className="card-actions">
-                            <button className="btn bg-red-500 text-white">Delete Movie</button>
+                            <button onClick={() => handleDelete(_id)} className="btn bg-red-500 text-white">Delete Movie</button>
                             <button className="btn bg-red-500 text-white">Add to Favorite</button>
+                        </div>
+                        <div className="card-actions justify-end mt-14">
+                            <button onClick={handleClick} className="btn btn-neutral">See All Movies</button>
                         </div>
                     </div>
                 </div>
